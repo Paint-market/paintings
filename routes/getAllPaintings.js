ca91550f8190e540872784e8b09ea268b932ea99
Apprentice <apprentice@StMac3-2.local>
2016-03-10 20:12:28 +1300

    Update before shutting dowon

--- routes/getAllPaintings.js
+++ routes/getAllPaintings.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var paintings = {
   paintings: [
  { paintingId: 1, creatorId: 1, ownerId: 1, title: 'Pixel', image: 'www.google.com/paintings', price: 55 },
- { paintingId: 2, creatorId: 2, ownerId: 2, title: 'Pixel v Vector', image: 'www.google.com/paintings2', price: 5 }
+ { paintingId: 2, creatorId: 2, ownerId: 2, title: 'Pixel v Vector', image: 'www.google.com/paintings2', price: 5 },
  { paintingId: 3, creatorId: 3, ownerId: 3, title: 'Vector', image: 'www.google.com/paintings3', price: 10}
   ]
 }
 /* GET all paintings. */
 router.get('/', function(req, res, next) {
-  //res.send('great art at affordable prices');
-  res.send(paintings.paintings);
+  res.send('great art at affordable prices');
+  //res.send(paintings.paintings);
 });
 
 module.exports = router;
