c1528b3fda31475aaa843047898b86d0ae1f7e2a
lou & Mel & Toby Johnston <pair+louise-r-blue.melissa-c.tobyjohnston38@devbootcamp.com>
2016-03-10 19:10:11 +1300

    add test file with draft code

---
+++ routes/createPainting.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(paintings);
+});
+
+var paintings = {
+ paintings: [
+  {paintingId: 1, creatorId: 11, ownerId: 111, title: 'depressed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636563386/', price: 100},
+  {paintingId: 2, creatorId: 22, ownerId: 222, title: 'disturbed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636574156/', price: 200},
+  {paintingId: 3, creatorId: 33, ownerId: 333, title: 'artsy fartsy', image: 'https://www.flickr.com/photos/sarahjohnston/5130007088/', price: 300}
+ ]
+}
+
+// app.get('/cats/new', function(req, res) {
+//  res.render('catsNew')
+// })
+
+// app.post('/cats', function(req,res) {
+//   //find all the cat ids, identify the largest and add 1 to get the new id
+//   //assign id to new cat details
+//   //add completed details to the cat array
+//   cats = addNewCat(cats, req.body);
+//   res.redirect('/cats')
+// })
+
+
+/*
+  // check the request params
+  // see if there's a cat for that id
+  // if there was ... return that cat object
+  // else error ? ...
+router.get('/:id', function(req, res, next){
+  var cat = cats.cats.find(findCatById)
+  function findCatById(cat){
+    return cat.id === parseInt(req.params.id)
+  }
+  if(cat === undefined){
+    res.status(404).send('Invalid ID');
+  } else {
+    res.json(cat)
+  }
+})
+*/
+module.exports = router;
--- test/testCreatePainting.js
+++ test/testCreatePainting.js
@@ -2,6 +2,39 @@ var request = require('supertest');
 var app = require('../app');
 var test = require('tape');
 
+test('create a new painting', function(t){
+  t.deepEqual(addNewPainting(paintingsSample, newPainting), expectedPaintings, 'The new painting has been added with a paintingId greater than the largest paintingId')
+  t.end()
+})
+
+var newPainting = {
+    paintingId: 4, creatorId: 44, ownerId: 444, title: 'I am new', image: 'https://www.flickr.com/photos/imagomundiphoto/25032203254/', price: 400
+    }
+
+var paintingsSample = {
+ paintings: [
+  {paintingId: 1, creatorId: 11, ownerId: 111, title: 'depressed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636563386/', price: 100},
+  {paintingId: 2, creatorId: 22, ownerId: 222, title: 'disturbed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636574156/', price: 200},
+  {paintingId: 3, creatorId: 33, ownerId: 333, title: 'artsy fartsy', image: 'https://www.flickr.com/photos/sarahjohnston/5130007088/', price: 300}
+ ]
+}
+
+var expectedPaintings = {
+ paintings: [
+  {paintingId: 1, creatorId: 11, ownerId: 111, title: 'depressed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636563386/', price: 100},
+  {paintingId: 2, creatorId: 22, ownerId: 222, title: 'disturbed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636574156/', price: 200},
+  {paintingId: 3, creatorId: 33, ownerId: 333, title: 'artsy fartsy', image: 'https://www.flickr.com/photos/sarahjohnston/5130007088/', price: 300},
+  {paintingId: 4, creatorId: 44, ownerId: 444, title: 'I am new', image: 'https://www.flickr.com/photos/imagomundiphoto/25032203254/', price: 400}
+ ]
+}
+
+function addNewPainting(paintingsSample, newPainting) {
+    var newPaintingId = paintingsSample.paintings.length + 1
+    newPainting.paintingId = newPaintingId
+    paintingsSample.paintings.push(newPainting)
+    return paintingsSample
+}
+
 /* get cat by id example
 test('get status code 200 from /v1/cats/:id', function(t) {
   request(app)
@@ -36,10 +69,6 @@ test('requesting invalid cat returns error', function(t){
 })*/
 
 /* get all cats example
-var request = require('supertest');
-var app = require('../app');
-var test = require('tape');
-
 
 test('get status code 200 from /v1/cats', function(t) {
   request(app)
@@ -61,7 +90,6 @@ test('get /v1/cats returns an object with the property "cats"', function(t) {
       t.end()
     })
 })
-
 */
 
 
