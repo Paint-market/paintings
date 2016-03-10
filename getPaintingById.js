6a5cac510a1183dc73ebb0d7bbb634067c79c4a3
toby.johnston <tobyjohnston38@gmail.com>
2016-03-11 10:22:29 +1300

    updated test with var data

--- getPaintingById.js
+++ getPaintingById.js
@@ -4,23 +4,6 @@ var express = require('express');
 var fs = require('fs');
 var morgan = require('morgan');
 
-// var routes = require('')
-// var user = require ('')
-
-var app = express()
-
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use('/v1/paintings', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 
 
--- test/testGetPaintingById.js
+++ test/testGetPaintingById.js
@@ -2,7 +2,20 @@ var request = require('supertest');
 var app = require('../app');
 var test = require('tape');
 
-test('get status code 200 from /v1/paintings/:id', function(t) {
+var expectedPaintings = {
+  paintings: [
+
+{ paintingId: 1, creatorId: 11, ownerId: 111, title: 'depressed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636563386/', price: 5 },
+{ paintingId: 2, creatorId: 22, ownerId: 222, title: 'disturbed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636574156/', price: 20 },
+{ paintingId: 3, creatorId: 33, ownerId: 333, title: 'artsy fartsy', image: 'https://www.flickr.com/photos/sarahjohnston/5130007088/', price: 300 },
+{ paintingId: 4, creatorId: 1, ownerId: 1, title: 'Pixel', image: 'www.google.com/paintings', price: 55 },
+{ paintingId: 5, creatorId: 2, ownerId: 2, title: 'Pixel v Vector', image: 'www.google.com/paintings2', price: 5 },
+{ paintingId: 7, creatorId: 3, ownerId: 3, title: 'Vector', image: 'www.google.com/paintings3', price: 10},
+{ paintingId: 8, creatorId: 2, ownerId: 1, title: 'Pixel2', image: 'www.google.com/paintings', price: 10}
+  ]
+}
+
+test('test', function(t) {
   request(app)
     .get('/v1/paintings/:id')
     .expect(200)
@@ -12,27 +25,6 @@ test('get status code 200 from /v1/paintings/:id', function(t) {
     })
 })
 
-test('get /v1/paintings/:id returns a paintings object', function(t) {
-  request(app)
-    .get('/v1/paintings/:id')
-    .expect(200)
-    .end(function(err, res) {
-      t.false(err, "no error")
-      t.true(typeof(res.body) === 'object', "the response returns an object")
-      t.end()
-    })
-})
-
-test('requesting invalid paintings returns error', function(t){
-  request(app)
-    .get('/v1/cats/900')
-    .expect(404)
-    .end(function(err, res) {
-      t.false(err, "this is the correct error code")
-      t.deepEqual(res.text, 'Invalid ID', "the response is an invalid id")
-      t.end()
-    })
-})
 
 
 
