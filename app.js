8a0e8546b44e23d676ec194ca47b4a01011c1eab
lou & Mel & Toby Johnston <pair+louise-r-blue.melissa-c.tobyjohnston38@devbootcamp.com>
2016-03-11 11:27:22 +1300

    Add changed file names and delete access files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var paintings = require('./routes/getAllPaintings');
+var paintings = require('./routes/index');
 
 var app = express();
 
@@ -14,39 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-app.use('/users', users);
-app.use('/', paintings);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
+app.use('/v1/users', users);
+app.use('/v1/paintings', paintings);
 
 module.exports = app;
--- getAllPaintingsTest/getAllPaintingsTest.js
+++ getAllPaintingsTest/getAllPaintingsTest.js
@@ -2,36 +2,16 @@ var request = require('supertest');
 var app = require('../app');
 var test = require('tape');
 
-var expectedPaintings = {
-  paintings: [
-
-{ paintingId: 1, creatorId: 11, ownerId: 111, title: 'depressed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636563386/', price: 5 },
-{ paintingId: 2, creatorId: 22, ownerId: 222, title: 'disturbed', image: 'https://www.flickr.com/photos/imagomundiphoto/25636574156/', price: 20 },
-{ paintingId: 3, creatorId: 33, ownerId: 333, title: 'artsy fartsy', image: 'https://www.flickr.com/photos/sarahjohnston/5130007088/', price: 300 },
-{ paintingId: 4, creatorId: 1, ownerId: 1, title: 'Pixel', image: 'www.google.com/paintings', price: 55 },
-{ paintingId: 5, creatorId: 2, ownerId: 2, title: 'Pixel v Vector', image: 'www.google.com/paintings2', price: 5 },
-{ paintingId: 7, creatorId: 3, ownerId: 3, title: 'Vector', image: 'www.google.com/paintings3', price: 10},
-{ paintingId: 8, creatorId: 2, ownerId: 1, title: 'Pixel2', image: 'www.google.com/paintings', price: 10}
-  ]
-}
-
-test('get status code 200 from /v1/paintings', function(t) {
+test('get status code 200 from /paintings', function(t) {
   request(app)
     .get('/v1/paintings')
     .expect(200)
     .end(function(err, res) {
-      t.false(err)
+      console.log("res :", res.body)
+      t.false(err, 'no error for false')
+      t.true(res.body.hasOwnProperty('paintings'), 'res.body returns an object that has a property called paintings')
+      t.equal(res.body.paintings.length, 7, "returns all paintings")
       t.end()
     })
 })
 
-// test('get /v1/paintings returns an objects with the property "paintings"', function(t) {
-//   request(app)
-//     .get('/v1/paintings')
-//     .expect(200)
-//     .end(function(err, res) {
-//       t.false(err)
-//       t.true(res.body.hasOwnProperty('paintings'))
-//       t.end()
-//     })
-// })
