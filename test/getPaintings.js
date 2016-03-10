7e3c985f83c9cdc6a0b3aefed242997f35b0630c
lou & Mel & Toby Johnston <pair+louise-r-blue.melissa-c.tobyjohnston38@devbootcamp.com>
2016-03-11 12:26:55 +1300

    delete unnecessary text

--- test/getPaintings.js
+++ test/getPaintings.js
@@ -7,7 +7,6 @@ test('get status code 200 from /paintings', function(t) {
     .get('/v1/paintings')
     .expect(200)
     .end(function(err, res) {
-      console.log("res :", res.body)
       t.false(err, 'no error for false')
       t.true(res.body.hasOwnProperty('paintings'), 'res.body returns an object that has a property called paintings')
       t.equal(res.body.paintings.length, 7, "returns all paintings")
