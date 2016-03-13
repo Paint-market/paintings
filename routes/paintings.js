aba157b3d679cdd4cd562deac4d7689b65ec424d
LucyMoore <lucyviolet.moore@gmail.com>
2016-03-13 13:33:12 +1300

    still stuck on object syntax

--- routes/paintings.js
+++ routes/paintings.js
@@ -10,26 +10,27 @@ router.get('/', function(req, res, next) {
   fs.readFile('DB.json', 'utf8', function(err, data){
   var paintings = JSON.parse(data)
 
-  //create object to hold returned items
+  //create object to hold returned results
   var paintingResults = []
+  //store search params from query
   var searchParams = Object.keys(req.query)
-
-
-  
 
   //if no search filter passed in return all paintings 
   if(searchParams ===[]){
     paintingResults = paintings
   }
+  //if search parameter is title
  else if(searchParams ==='title'){
-  //title search requires special consideration
+  //title search requires separate function
     
   }
   else {
     //search database by search params
-    paintings.paintings.forEach(function(painting){
-      if(painting.creatorId===11){
+    paintings.paintings.forEach(function(painting){//loop through each object in array
+      console.log(painting[searchParams[0]], '**')
+      if(painting[searchParams]===req.query[searchParams]){
       paintingResults.push(painting)
+
     }
      })
     console.log(paintingResults)
