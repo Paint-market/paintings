998404d3a9ea7daa729f30577403720bcea29789
Daniel <danieldelacruz01@gmail.com>
2016-03-13 18:09:59 +1300

    add redirect

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-
+var routes = require('./routes/index');
 var paintings = require('./routes/v1/paintings');
 
 var app = express();
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+app.use('/', routes);
 app.use('/v1/paintings', paintings);
 
 module.exports = app;
