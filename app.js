1281a7dc389179c7ff950a93c4f387364a55cc43
Piet Geursen <pietgeursen@gmail.com>
2016-03-14 18:20:23 +1300

    Add cors module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 
 var paintings = require('./routes/v1/paintings');
@@ -11,6 +12,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
 
 
 app.use('/v1/paintings', paintings);
